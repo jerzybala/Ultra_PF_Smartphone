8a3ec38b14ddbe437ec0345473e5423275e5bfbb
jerzybala <64710285+jerzybala@users.noreply.github.com>
2024-01-28 22:58:38 -0500

    modified:   .DS_Store 	deleted:    Shap-analysis-1.py 	modified:   Shap_analysis_UPF.ipynb 	new file:   __pycache__/config.cpython-39.pyc 	new file:   config.py

---
+++ config.py
@@ -0,0 +1,152 @@
+
+# config.py
+
+region_mapping = {
+    'Anglosphere': ['United States', 'Canada', 'United Kingdom', 'Ireland', 'New Zealand', 'Australia'],
+    'Latin America': ['Argentina', 'Chile', 'Colombia', 'Ecuador', 'Guatemala', 'Mexico', 'Peru', 'Puerto Rico', 'Venezuela', 'Brazil', 'Bolivia', 'Paraguay', 'Uruguay'],
+    'Middle East': ['Iraq', 'Saudi Arabia', 'United Arab Emirates', 'Yemen', 'Iran', 'Israel', 'Jordan', 'Kuwait', 'Lebanon', 'Oman', 'Qatar', 'Syria', 'Bahrain'],
+    'French & Spanish Speaking Mainland Europe': ['Spain', 'France', 'Belgium', 'Switzerland', 'Portugal', 'Italy', 'Greece', 'Germany', 'Austria', 'Netherlands'],
+    'West Africa': ['Cameroon', 'Cote d’Ivoire', 'Democratic Republic of the Congo', 'Nigeria', 'Ghana', 'Senegal', 'Mali', 'Niger', 'Guinea'],
+    'North Africa': ['Algeria', 'Egypt', 'Morocco', 'Tunisia', 'Libya', 'Sudan'],
+    'East & Southeast Asia': ['China', 'Japan', 'South Korea', 'North Korea', 'Vietnam', 'Thailand', 'Malaysia', 'Indonesia', 'Philippines', 'Singapore'],
+    'South Asia': ['India', 'Pakistan', 'Bangladesh', 'Sri Lanka', 'Nepal', 'Bhutan'],
+    'Eastern Europe & Central Asia': ['Russia', 'Ukraine', 'Belarus', 'Moldova', 'Armenia', 'Azerbaijan', 'Georgia', 'Kazakhstan', 'Uzbekistan', 'Turkmenistan', 'Kyrgyzstan', 'Tajikistan'],
+    'Scandinavia & Baltic': ['Sweden', 'Norway', 'Finland', 'Denmark', 'Iceland', 'Estonia', 'Latvia', 'Lithuania'],
+    'Oceania & Pacific Islands': ['Fiji', 'Papua New Guinea', 'Samoa', 'Solomon Islands', 'Tonga'],
+    'Sub-Saharan Africa': ['Kenya', 'Uganda', 'Tanzania', 'Rwanda', 'Burundi', 'South Africa', 'Zimbabwe', 'Zambia', 'Botswana', 'Namibia', 'Mozambique', 'Madagascar'],
+    'Central America & Caribbean': ['Cuba', 'Dominican Republic', 'Haiti', 'Jamaica', 'Trinidad and Tobago', 'Barbados', 'Saint Lucia', 'Grenada', 'Belize', 'Costa Rica', 'El Salvador', 'Honduras', 'Nicaragua', 'Panama']
+    # ... You can continue to add more regions and countries as needed.
+}
+
+
+
+set1 = [
+ 'Overall MHQ',
+ 'MHQ_Sign',
+ 
+ 'Age',
+ 'Biological Sex',
+#  'Different gender from biological sex',
+#  'Gender Identity',
+#  'Ethnicity',
+ 'Country',
+#  'State',
+#  'Place of living',
+#  'City',
+ 'Education',
+ 'Employment',#  Yes
+ 'Region',
+#  'Profession/Employment Sector',
+ 
+ 'Frequency of getting a good nights sleep',
+ 'Frequency of doing exercise',
+ 'Processed food in diet',
+ 'Frequency of Socializing',
+ 
+#  'Number of Children',
+#  'Household Size',
+#  'Number of Siblings when growing up',
+#  'Number of Close Friends',
+ 
+#  'Description of Household Growing up',
+#  'Relationship with Adult Family',
+#  'Spirituality connection',
+#  'Feelings of love towards others',
+#  'Particular religion identity',
+#  'Religion practice',
+
+# smartphone with age<24
+ 
+#  'Smartphone allowed in school',
+#  'Smartphone use in lessons',
+#  'Age of first social media account',
+#  'Frequency of social media posting',
+#  'Tablet ownership',
+#  'Smartphone ownership',
+#  'Friends/classmates smarphone ownership',
+#  'Age of smartphone usage during school hours',
+#  'Smartphone usage during class hours',
+#  'Smartphone usage during break',
+#  'Age of laptop usage required by school',
+#  'Laptop usage for non-learning activities',
+
+ 'Sudden or premature death of a loved one',
+ 'Divorce/separation  or family breakup',
+ 'Extreme poverty leading to homelessness and/or hunger.',
+ 'Forced family control over major life decisions (e.g. marriage)',
+ 'Prolonged sexual abuse| or severe sexual assault.',
+ 'Displacement from your home due to political| environmental or economic reasons',
+ 'Loss of your job or livelihood leading to an inability to make ends meet.',
+ 'Cyberbullying or online abuse',
+ 'Threatening| coercive or controlling behavior by another person',
+ 'Caring for a child or partner with a major chronic disability or illness',
+ 'I did not experience any of the above',
+ 'Involvement or close witness to a war',
+ 'Life threatening or debilitating injury or illness.',
+ 'Suffered a loss in a major fire| flood| earthquake| or natural disaster',
+ 'None of the above AT',
+
+#  'Type II Diabetes',
+#  'Fibromyalgia',
+#  'Liver disease/Cirrhosis',
+#  'Hypertension',
+#  'Irritable Bowel Syndrome',
+#  'Heart disease',
+#  "Inflammatory Bowel Disease / Crohn's disease",
+#  'Arthritis',
+#  'Psoriasis',
+#  'Asthma',
+#  'Migraines',
+#  'Traumatic Brain Injury',
+#  'Osteoporosis',
+#  'Sleep apnea',
+#  'Hipotiroidismo',
+#  'Neuropathy',
+#  'Cancer',
+#  'Chronic Obstructive Pulmonary Disease (COPD)',
+#  'HIV /AIDS',
+#  'Kidney Disease',
+#  'Rheumatoid',
+#  'Polycystic ovaries',
+#  'Prefer not to say',
+#  'Narcolepsy',
+#  'Chronic fatigue syndrome',
+#  'Back problem',
+#  'Epilepsy',
+#  'Multiple sclerosis',
+#  'Escoliosis',
+#  'Stroke',
+#  'Herpes',
+#  'Type 1 Diabetes',
+
+ 'Sudden or premature death of a parent or sibling',
+ 'Prolonged emotional or psychological abuse or neglect from parent/caregiver',
+ 'Prolonged physical abuse| or severe physical assault CT',
+ 'Physical violence in the home between family members',
+ 'Prolonged or sustained bullying in person from peers',
+ 'Parental Divorce or family breakup',
+ 'Lived with a parent/caregiver who was an alcoholic or who regularly used street drugs',
+ 'Threatening| coercive or controlling behavior by another person CT',
+ 'I did not experience any of the above during my childhood',
+ 'Suffered a loss in a major fire| flood| earthquake| or natural disaster CT',
+ 'Displacement from your home due to political| environmental or economic reasons CT',
+ 'Life threatening or debilitating injury or illness CT',
+ 'Forced family control over major life decisions CT',
+ 'None of the above CT',
+
+ 'Tobacco products',
+ 'Alcoholic beverages',
+ 'Cannabis',
+ 'Vaping products',
+ 'Sedatives or Sleeping Pills',
+ 'Amphetamine type stimulants (e.g. speed| diet pills| ecstasy| etc.)',
+ 'Opioids',
+ 'Melatonina',
+ 'Cigarrillo',
+ 'None of the above']
+
+
+
+
+
+
